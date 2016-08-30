bfaab12ea9113cf66ee3d49fa51052c858ec4559
C J Silverio <ceejceej@gmail.com>
2016-08-29 21:11:19 -0700

    Revert previous commit.

---
+++ README.md
@@ -0,0 +1,22 @@
+# nms-species-bot
+
+No Person's Sky species generation bot.
+
+## Usage
+
+Clone the repo; run `npm install`. Create a file named `.env` in the repo directory and put your Twitter API access tokens in it like so:
+
+```sh
+TWITTER_CONSUMER_KEY=your-key
+TWITTER_CONSUMER_SECRET=your-secret
+TWITTER_ACCESS_TOKEN=you-token
+TWITTER_ACCESS_TOKEN_SECRET=your-secret
+```
+
+Optionally, make a subdirectory named `images`.
+
+Then run `npm start`. Your bot will now be online.
+
+## License
+
+ISC
---
+++ server.js
@@ -0,0 +1,124 @@
+#!/usr/bin/env node
+
+require('dotenv').config();
+var
+	fs        = require('fs'),
+	shuffle   = require('knuth-shuffle').knuthShuffle,
+	Twit      = require('twit')
+	;
+
+const INTERVAL = 180 * 60 * 1000; // once every 3 hours
+const LAST_POST_FILE = '.lastpost';
+
+function log(msg)
+{
+	console.log([new Date(), msg].join(' '));
+}
+
+function readOptions(which)
+{
+	var lines = fs.readFileSync('./' + which + '.txt', 'ascii').trim().split('\n');
+	log(['---', lines.length, which, 'read'].join(' '));
+	return lines;
+}
+
+function getMeOne(which)
+{
+	shuffle(DATA[which]);
+	return DATA[which][0];
+}
+
+var DATA = {
+	ages:         readOptions('ages'),
+	genders:      readOptions('genders'),
+	temperaments: readOptions('temperaments'),
+	diets:        readOptions('diets'),
+	images:       readOptions('images'),
+};
+
+var config = {
+	consumer_key:         process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
+	access_token:         process.env.TWITTER_ACCESS_TOKEN,
+	access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
+	timeout_ms:           60 * 1000,  // optional HTTP request timeout to apply to all requests.
+};
+var T = new Twit(config);
+
+function postTweet(toot)
+{
+	T.post('statuses/update', toot, function handleTootResponse(err, data, res)
+	{
+		if (err)
+			log(err);
+		else
+		{
+			log('tweet id=' + data.id_str + '; ' + toot.status);
+			fs.writeFileSync(LAST_POST_FILE, (new Date()).toString());
+		}
+	});
+}
+
+// Mentions as a stream.
+var mentions = T.stream('user');
+mentions.on('tweet', function handleMention(tweet)
+{
+	if (tweet.in_reply_to_screen_name !== 'malcolm_ebooks')
+		return;
+
+	var status = `@${tweet.user.screen_name}: Your species is like this:
+
+${buildSpecies()}`;
+
+	var toot = {
+		status: status,
+		in_reply_to_status_id: tweet.id_str
+	};
+
+	postTweet(toot);
+});
+
+mentions.on('error', function handleMentionsError(err)
+{
+	log('mentions error: ' + err);
+});
+
+function buildSpecies()
+{
+	var result = `age: ${getMeOne('ages')}`;
+
+	if (result.length < 140)
+		result += '\ngender: ' + getMeOne('genders');
+
+	if (result.length < 140)
+		result += '\ntemperament: ' + getMeOne('temperaments');
+
+	if (result.length < 140)
+		result += '\ndiet: ' + getMeOne('diets');
+
+	if (result.length < 108)
+		result += '\n' + getMeOne('images');
+
+	return result;
+}
+
+function postPeriodically()
+{
+	postTweet({ status: buildSpecies() });
+}
+
+log('no person\'s sky species bot online');
+
+var postNow = false;
+try
+{
+	var lastPost = new Date(fs.readFileSync(LAST_POST_FILE, 'ascii'));
+	postNow = (Date.now() - lastPost.getTime()) >= INTERVAL / 2;
+}
+catch (e)
+{
+	postNow = true;
+}
+
+if (postNow) postPeriodically();
+setInterval(postPeriodically, INTERVAL);
